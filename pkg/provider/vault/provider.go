f6b7ada89d384172794605472b561ca0d642a718
Bence Csati <bcsati@cisco.com>
2024-06-03 18:10:12 +0200

    chore: add comment to agent configs
    Signed-off-by: Bence Csati <bcsati@cisco.com>

--- pkg/provider/vault/provider.go
+++ pkg/provider/vault/provider.go
@@ -25,6 +25,17 @@ import (
 )
 
 const (
+	// AgentConfig is a string that represents the configuration for the Vault agent.
+	// It includes the path to the pid file, and the configuration for automatic authentication.
+	// The automatic authentication is configured to use the Kubernetes method,
+	// with a specified namespace, mount path, and role.
+	// The configuration also includes a sink of type "file", which specifies the path
+	// to the token that the Vault agent will use for authentication.
+	// This part of the code is called when the 'UseAgent' annotation is set to true.
+	// When 'UseAgent' is true, the Vault agent is used for handling Vault authentication and token renewal.
+	// This ConfigMap, created in the active selection, is necessary for its configuration.
+	// It sets the name of the ConfigMap, assigns the owner references, and populates the data with the agent configuration.
+	// The agent configuration is formatted with the Vault namespace, path, and role specified in the manager's configuration.
 	AgentConfig = `
 	pid_file = "/tmp/pidfile"
 	
